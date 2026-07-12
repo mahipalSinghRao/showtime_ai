21a41ab1f34b116f0f4d0ac26e7696f2000050c7
Mahipal Singh <mahipalsingh450@gmail.com>
2026-07-12 11:49:28 +0530

    test: remove placeholder tests and fix movie route

--- src/tests/movie/movie.test.ts
+++ src/tests/movie/movie.test.ts
@@ -1,24 +1,57 @@
 import request from "supertest";
 import app from "@/app";
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
+
+// describe("Movie API", () => {
+
+//     beforeAll(async () => {
+//         console.log("Mongo Ready:", mongoose.connection.readyState);
+//     });
+
+//     it("should return movies", async () => {
+
+//         const response = await request(app)
+//             .get("/api/v1/movies");
+
+//         console.log("Mongo Ready:", mongoose.connection.readyState);
+//         console.log(response.body);
+
+//         expect(response.status).toBe(200);
+//         expect(response.body).toMatchObject({
+//             success: true,
+//             statusCode: 200,
+//         });
+
+//         expect(Array.isArray(response.body.data.movies)).toBe(true);
+//     });
+// });
 
 describe("Movie API", () => {
-
-    beforeAll(async () => {
-        console.log("Mongo Ready:", mongoose.connection.readyState);
-    });
 
     it("should return movies", async () => {
 
         const response = await request(app)
             .get("/api/v1/movies");
 
-        console.log("Mongo Ready:", mongoose.connection.readyState);
-        console.log(response.body);
-
         expect(response.status).toBe(200);
-        expect(response.body.success).toBe(true);
-        expect(Array.isArray(response.body.data)).toBe(true);
+
+        expect(response.body).toMatchObject({
+            success: true,
+            statusCode: 200,
+            message: "Movies fetched successfully",
+        });
+
+        expect(Array.isArray(response.body.data.movies)).toBe(true);
+
+        expect(response.body.data.pagination).toMatchObject({
+            page: expect.any(Number),
+            limit: expect.any(Number),
+            total: expect.any(Number),
+            totalPage: expect.any(Number),
+            hasNextPage: expect.any(Boolean),
+            hasPreviousPage: expect.any(Boolean),
+        });
+
     });
 
 });
